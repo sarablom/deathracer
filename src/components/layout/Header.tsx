89a7662d1dacd5f020bd161bf615a25c7045ead0
Sara Blom <sara.cecilia.blom@gmail.com>
2023-06-10 13:02:50 +0200

    create a nav component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,39 +1,16 @@
-import { FC } from "react";
-import Link from "next/link";
 import styled from "styled-components";
+import { Nav } from "./Nav";
 
-export const Header: FC = () => {
-    return (
-        <HeaderWrapper>
-            <NavWrapper>
-                <Link href="/about">About</Link>
-                <Link href="/music">Music</Link>
-                <Link href="/shows">Shows</Link>
-                <Link href="/merch">Merch</Link>
-            </NavWrapper>
-        </HeaderWrapper>
-    );
+export const Header = () => {
+	return (
+		<HeaderWrapper>
+			<Nav />
+		</HeaderWrapper>
+	);
 };
 const HeaderWrapper = styled.header`
-    display: flex;
-    justify-content: space-between;
-    background: var(--color-secondary);
-`;
-
-const NavWrapper = styled.nav`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 5rem;
-    gap: 3rem;
-    a {
-        text-decoration: none;
-        color: var(--color-blackish);
-        cursor: pointer;
-        &:hover {
-            color: var(--color-whiteish);
-            background: var(--color-secondary);
-        }
-    }
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	background: var(--color-secondary);
 `;
