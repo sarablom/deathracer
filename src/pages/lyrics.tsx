3f61cd619bd8c0de0c2d04d5cbd0d4f4ebcbae0c
Sara Blom <sara.cecilia.blom@gmail.com>
2023-06-10 13:04:19 +0200

    create a lyrics page

---
+++ src/pages/lyrics.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+export const Lyrics: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Deathracer --- Lyrics</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1>Lyrics</h1>
+            <p>Content coming soon</p>
+        </>
+    );
+};
+
+export default Lyrics;
