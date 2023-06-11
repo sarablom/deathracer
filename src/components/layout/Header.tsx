103066e1b1eeb5d040305036d0bf8e5b80023ab6
Sara Blom <sara.cecilia.blom@gmail.com>
2023-06-11 09:27:54 +0200

    update death logo in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Nav } from "../Nav";
-import logo from "../../../public/images/death-logo-1.svg";
+import logo from "../../../public/images/death-logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -24,4 +24,10 @@ const HeaderWrapper = styled.header`
 	width: 100%;
 	background: var(--color-secondary);
 	padding: 16px 32px;
+
+	img {
+		position: absolute;
+		top: 16px;
+		left: 32px;
+	}
 `;
