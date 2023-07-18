613b7065e1cc30a89a3745f33575201226bcb4cb
Sara Blom <sara.cecilia.blom@gmail.com>
2023-07-18 22:53:00 +0200

    update order of imports

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,12 @@
 "use client";
-import styled from "styled-components";
-import { Nav } from "../Nav";
-import logo from "../../../public/images/death-logo.svg";
 import Image from "next/image";
 import Link from "next/link";
+
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
+import { Nav } from "../Nav";
+
+import logo from "../../../public/images/death-logo.svg";
+import styled from "styled-components";
 
 export const Header = () => {
 	const { width } = useWindowDimensions();
