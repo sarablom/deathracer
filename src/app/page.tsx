637d04be9d6cc43f4223115d5f5abdcc09b8cb00
Sara Blom <sara.cecilia.blom@gmail.com>
2023-07-21 08:09:15 +0200

    update import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import logo from "../../public/images/logo.jpg";
-import Image from "next/image";
-import styles from "./styles.module.css";
+import { CarImage } from "./StyledComponents";
 import { Metadata, NextPage } from "next";
 
 export const metadata: Metadata = {
@@ -10,8 +9,7 @@ export const metadata: Metadata = {
 
 const Home: NextPage = () => {
 	return (
-		<Image
-			className={styles.carLogo}
+		<CarImage
 			src={logo}
 			alt="Deathracer logo - a Volvo 142 driven by a skeleton with the registration plate DEATH"
 		/>
