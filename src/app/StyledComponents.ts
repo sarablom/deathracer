cdd688072a6b0f0f1dc786cb6142d4a9a11ecbb7
Sara Blom <sara.cecilia.blom@gmail.com>
2023-07-21 08:07:40 +0200

    replace css modules with styled components

---
+++ src/app/StyledComponents.ts
@@ -0,0 +1,23 @@
+"use client";
+import Image from "next/image";
+import styled from "styled-components";
+
+export const CarImage = styled(Image)`
+	animation: drivinFromRight 500ms ease-in forwards;
+	width: 80%;
+	max-width: 750px;
+	height: fit-content;
+	margin: 0 auto;
+
+	@keyframes drivinFromRight {
+		from {
+			transform: translate(175px, -100px);
+			opacity: 0;
+		}
+
+		to {
+			transform: translate(35px, 15px);
+			opacity: 1;
+		}
+	}
+`;
