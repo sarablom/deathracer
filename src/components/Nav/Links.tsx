43ede5346959f7ef5bf29b0d631f089c4a51fe4b
Sara Blom <sara.cecilia.blom@gmail.com>
2023-07-24 10:59:53 +0200

    update hover and focus state for links

--- src/components/Nav/Links.tsx
+++ src/components/Nav/Links.tsx
@@ -34,10 +34,16 @@ export default Links;
 const NavbarLink = styled(Link)`
 	&:hover {
 		font-weight: 700;
-		text-decoration: underline overline;
+		text-decoration: underline;
 
 		@media only screen and (min-width: 801px) {
 			transform: scale(1.1);
 		}
+	}
+	&:active,
+	&:focus {
+		font-size: 1.3rem;
+		font-weight: 700;
+		text-decoration: 1.5px var(--color-success) wavy underline;
 	}
 `;
